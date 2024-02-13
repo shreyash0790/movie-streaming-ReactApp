0e746d02240c22df452802061bad3e1fa4df9772
Shreyaskar <shreyash0790@gmail.com>
2024-02-14 01:03:59 +0530

    ui update added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
 
 
-  async function deleteHandler(movie) {
-    try{
-    const response=await fetch(`https://react-movies-3a05d-default-rtdb.firebaseio.com/movies/${props.id}.json`,{
-   method:'DELETE',
-   
-    })
-   
-    if (response.ok) {
-      console.log(`Document with ID ${props.id} deleted successfully`);
-    } else {
-      console.error(`Failed to delete document with ID ${props.id}`);
-    }
-   
-    }catch(err){
-     console.log(err)
-    }
-   }
+  const [isDeleted, setIsDeleted] = useState(false)
 
+  async function deleteHandler() {
+    try {
+      const response = await fetch(`https://react-movies-3a05d-default-rtdb.firebaseio.com/movies/${props.id}.json`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        console.log(`Document with ID ${props.id} deleted successfully`);
+        setIsDeleted(true); 
+      
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+ 
+  if (isDeleted) {
+    return null;
+  }
 
 
 
